ade50ffd43691af70f7447610957cb9782e1d460
Dmitriy <sctoss@gmail.com>
2022-09-03 14:55:26 +0300

    created function printarray

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,4 +1,14 @@
 ﻿// Написать программу, которая из имеюущегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символам.
+
+string[] PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    return array;
+}
+
 Console.Write("Введите количество элементов массива:\t");
 int elementCount = int.Parse(Console.ReadLine());
 string[] myArray = new string[elementCount];
@@ -7,12 +17,10 @@ for (int i = 0; i < myArray.Length; i++)
     Console.Write($"Введите элемент №{i + 1}: ");
     myArray[i] = Console.ReadLine();
 }
-Console.Write("\nМассив: ");
-for (int i = 0; i < myArray.Length; i++)
-{
-    Console.Write($"{myArray[i]} ");
-}
-Console.WriteLine("\nМассив с элементами длиной меньше 3 символов: ");
+Console.Write("\nВведенный массив: ");
+PrintArray(myArray);
+
+Console.Write("\nНовый массив с элементами длиной меньше либо равный 3 символам: ");
 int count = 0;
 string[] newArray = new string[myArray.Length];
 for (int i = 0; i < myArray.Length; i++)
