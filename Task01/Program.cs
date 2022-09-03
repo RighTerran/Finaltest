ad356afcb377852689c777e49b68504344591ee5
Dmitriy <sctoss@gmail.com>
2022-09-03 14:43:28 +0300

    created new array str.17

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,10 +1,10 @@
-﻿
+﻿// Написать программу, которая из имеюущегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символам.
 Console.Write("Введите количество элементов массива:\t");
 int elementCount = int.Parse(Console.ReadLine());
 string[] myArray = new string[elementCount];
 for (int i = 0; i < myArray.Length; i++)
 {
-    Console.WriteLine($"Введите элемент №{i + 1}: ");
+    Console.Write($"Введите элемент №{i + 1}: ");
     myArray[i] = Console.ReadLine();
 }
 Console.Write("\nМассив: ");
@@ -14,13 +14,15 @@ for (int i = 0; i < myArray.Length; i++)
 }
 Console.WriteLine("\nМассив с элементами длиной меньше 3 символов: ");
 int count = 0;
+string[] newArray = new string[myArray.Length];
 for (int i = 0; i < myArray.Length; i++)
 {
     if (myArray[i].Length <= 3)
     {
-        Console.Write(myArray[i] + " ");
+        newArray[i] = myArray[i];
+        Console.Write(newArray[i] + " ");
         count++;
     }
 }
 if (count == 0)
-    Console.WriteLine("Ошибка");
+    Console.WriteLine("Ошибка! Элементов длиной меньше 3 символов не обнаружено");
